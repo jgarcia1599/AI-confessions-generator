b7a042fe0eba78b9a2a4f7089cff34850fa229f4
Estelle Ocran <ocranestelle@gmail.com>
2020-11-17 11:09:20 +0400

    remove # sign

--- confessions_scraper.py
+++ confessions_scraper.py
@@ -10,20 +10,31 @@ python3 confessions_scraper.py
 
 from facebook_scraper import get_posts
 import emoji
-
+import io
+import re
 
 filename = 'confessions.txt'
-def give_emoji_free_text(text):
+def process_text(text):
     allchars = [str for str in text]
     emoji_list = [c for c in allchars if c in emoji.UNICODE_EMOJI]
     clean_text = ' '.join([str for str in text.split() if not any(i in str for i in emoji_list)])
+    processed_text = ''
+    try:
+        processed_text = re.split('"', clean_text)[1]
+        
+    except:
+        pass
+    return processed_text
 
-    return clean_text
-for post in get_posts('NYUAD-Crushes-and-Compliments-1185494961476520', pages=200):
+posts = []
+for post in get_posts('NYUAD-Crushes-and-Compliments-1185494961476520', pages=400):
     # remove emojis from text
-    clean_text = give_emoji_free_text(post['text'])
-    print(clean_text)
+    clean_text = process_text(post['text'])
+    
+    if clean_text != '':
+        posts.append(clean_text)
     # save cleantext in .txt file
-    with open(filename, 'a') as f:
-        print(give_emoji_free_text(clean_text),file=f)
-
+with io.open(filename, 'w', encoding="utf-8") as out_file:
+#     print(give_emoji_free_text(clean_text),file=f)
+    for post in posts:
+        out_file.write(post + '\n\n\n\n\n')
