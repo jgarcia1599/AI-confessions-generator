a9b10700ff1a5096bdf1066921451b4c650280bd
Estelle <eeo259@nyu.edu>
2020-11-23 13:22:05 +0400

    add code to generate text

--- app/app.py
+++ app/app.py
@@ -1,15 +1,60 @@
-from flask import Flask, render_template, request
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+import gpt_2_simple as gpt2
+import tensorflow as tf
+import uvicorn
+import os
+import gc
+import re
+app = Starlette(debug=False)
+
+sess = gpt2.start_tf_sess(threads=1)
+gpt2.load_gpt2(sess)
+
+# Needed to avoid cross-domain issues
+response_header = {
+    'Access-Control-Allow-Origin': '*'
+}
+
+generate_count = 0
 
 
+@app.route('/', methods=['GET', 'POST', 'HEAD'])
+async def homepage(request):
+    global generate_count
+    global sess
 
-app = Flask(__name__)
+    if request.method == 'GET':
+        params = request.query_params
+    elif request.method == 'POST':
+        params = await request.json()
+    elif request.method == 'HEAD':
+        return JSONResponse({'text': ''},
+                             headers=response_header)
+    print('+++++++++++++++')
+    print(params)
+    text = gpt2.generate(sess,
+                         length=100,
+                         temperature=float(params.get('temperature', 0.7)),
+                         prefix=params.get('prefix', '')[:500],
+                         return_as_list=True
+                         )[0]
 
+    generate_count += 1
+    if generate_count == 8:
+        # Reload model to prevent Graph/Session from going OOM
+        tf.reset_default_graph()
+        sess.close()
+        sess = gpt2.start_tf_sess(threads=1)
+        gpt2.load_gpt2(sess)
+        generate_count = 0
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+    gc.collect()
+    print('---------------')
+    text = re.split('\n', text)
+    print(text)
+    return JSONResponse({'text': text},
+                         headers=response_header)
 
-
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
